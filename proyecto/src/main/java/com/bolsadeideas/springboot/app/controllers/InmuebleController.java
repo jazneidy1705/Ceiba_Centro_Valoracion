6a760200acca1ca30d7ec349c4e93d148a72a2fa
jazneidy1705 <41234641+jazneidy1705@users.noreply.github.com>
2018-07-15 00:08:27 +0200

    Se sube enun tipo de vivienda

---
+++ proyecto/src/main/java/com/bolsadeideas/springboot/app/controllers/InmuebleController.java
@@ -0,0 +1,13 @@
+package com.bolsadeideas.springboot.app.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+@Controller
+@SessionAttributes("Inmueble")
+public class InmuebleController {
+
+	
+	
+	
+}
