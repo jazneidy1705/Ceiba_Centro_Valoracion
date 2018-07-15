42018d87e97d764955b1e94e285bd9bae0789a59
comando8124 <41245821+comando8124@users.noreply.github.com>
2018-07-15 00:15:23 -0500

    interface IInmuebleDao

--- proyecto/src/main/java/com/bolsadeideas/springboot/app/models/dao/IInmuebleDao.java
+++ proyecto/src/main/java/com/bolsadeideas/springboot/app/models/dao/IInmuebleDao.java
@@ -4,6 +4,7 @@ import org.springframework.data.repository.CrudRepository;
 
 import com.bolsadeideas.springboot.app.models.entity.Inmueble;
 
+//a
 public interface IInmuebleDao extends CrudRepository<Inmueble, Long> {
 
 }
